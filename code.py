6464bfac40d8b0b816e64846b9378b478c471c38
eeb110 <122307401+eeb110@users.noreply.github.com>
2023-02-17 10:30:12 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print("Name: " + name + " Age: " + age)
-=======
 name = input("Please enter your name: ")
 age = int(input("Please enter your age: "))
->>>>>>> 2755b2257e1bf5d46f1a25a758fce85f193f09b4
+print("Name: " + name + " Age: " + age)
